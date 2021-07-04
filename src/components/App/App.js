3b83d37451a75559aaee23884a0082dc5c596b48
algosketch <sjy_9484@naver.com>
2021-07-05 01:34:58 +0900

    refactor base code

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import logo from '../../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <input type="file"/>
+      <button>Youtube URL</button>
+    </main>
+  );
+}
+
+export default App;
