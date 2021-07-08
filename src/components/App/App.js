5873f71a9e9de3df545a2b500534b9a21fbf723d
algosketch <sjy_9484@naver.com>
2021-07-09 01:02:43 +0900

    align header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,32 +11,36 @@ function App() {
     <BrowserRouter>
       <Header/>
 
-      <main>
-        <input type="file" accept="audio/*, video/*"/>
-        <button onClick={openDialog}>Youtube URL</button>
-        {/* input youtube url */}
-        <dialog id={"inputURLDialog"}>
-          <form name="youtube-form" method="get" action="">
-            <p>dialog</p>
-            <input placeholder="Youtube URL" name="url"/>
+      <div className="wrap">
+        <main>
+          <input type="file" accept="audio/*, video/*"/>
+          <button onClick={openDialog}>Youtube URL</button>
+          { /* input youtube url */ }
+          <dialog id={"inputURLDialog"}>
+            <form name="youtube-form" method="get" action="">
+              <p>dialog</p>
+              <input placeholder="Youtube URL" name="url"/>
 
-            <Link to="/youtube">제출</Link>
-            <button value="cancel">취소</button>
-          </form>
-        </dialog>
+              <Link to="/youtube">제출</Link>
+              <button value="cancel">취소</button>
+            </form>
+          </dialog>
 
-        <div className="player-container">
-          <Link to="/video">비디오 플레이어</Link>
-          <Link to="/audio">오디오 플레이어</Link>
-          <Link to="/youtube">유튜브 플레이어</Link>
-          <Switch>
-            <Route path="/video" component={VideoPlayer}/>
-            <Route path="/audio" component={AudioPlayer}/>
-            <Route path="/youtube" component={YoutubePlayer}/>
-            <Route path="/youtube/:url" component={YoutubePlayer}/>
-          </Switch>
-        </div>
-      </main>
+          <div className="player-container">
+            <Link to="/video">비디오 플레이어</Link>
+            <Link to="/audio">오디오 플레이어</Link>
+            <Link to="/youtube">유튜브 플레이어</Link>
+            <Switch>
+              <Route path="/video" component={VideoPlayer}/>
+              <Route path="/audio" component={AudioPlayer}/>
+              <Route path="/youtube" component={YoutubePlayer}/>
+              <Route path="/youtube/:url" component={YoutubePlayer}/>
+            </Switch>
+          </div>
+        </main>
+      </div>
+
+
     </BrowserRouter>
   );
 }
--- src/global/Header/Header.js
+++ src/global/Header/Header.js
@@ -7,16 +7,23 @@ import Signup from "../../components/Signup/Signup";
 function Header() {
   return (
     <header>
-      <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/login">Log in</Link>
-        <Link to="/signup">Sign up</Link>
-        <Switch>
-          <Route path="/" component={Login}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/signup" component={Signup}/>
-        </Switch>
-      </BrowserRouter>
+      <nav>
+        <div className="link">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="link">
+          <Link to="/login">Log in</Link>
+        </div>
+        <div className="link">
+          <Link to="/signup">Sign up</Link>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/" component={Login}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/signup" component={Signup}/>
+      </Switch>
     </header>
   );
 }
