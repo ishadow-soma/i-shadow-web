ecd9f3130e23454be9494809e3f8fb462f39b152
algosketch <sjy_9484@naver.com>
2021-07-10 01:22:04 +0900

    cdd for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,31 @@
 import './Login.css';
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
 function Login() {
   return (
     <div className="login">
       <h2>Sign in to I-Shadow</h2>
-      <form action="" method="POST">
-        <label htmlFor="">ID</label>
-        <br/>
-        <input type="text"/>
-        <br/>
-        <label htmlFor="">Password</label>
-        <br/>
-        <input type="password"/>
-        <br/>
-        <button type="submit">Sign in</button>
-      </form>
-      <button>
-        sign in with google
-      </button>
+      <div className="login-form">
+        <form id="login-form" action="" method="POST">
+          <label htmlFor="">Email</label>
+          <br/>
+          <input type="text"/>
+          <br/>
+          <label htmlFor="">Password</label>
+          <br/>
+          <input type="password"/>
+          <br/>
+          <button type="submit" className="btn_submit">Sign in</button>
+        </form>
+      </div>
+
+      <div className="login-with-sns">
+        <FcGoogle id="google-icon"/>
+        <button>
+          sign in with google
+        </button>
+      </div>
     </div>
   );
 }
