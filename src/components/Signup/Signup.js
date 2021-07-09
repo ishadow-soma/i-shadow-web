c32d005fdf68836f482222ed7325f4d7b607aec3
algosketch <sjy_9484@naver.com>
2021-07-10 01:27:01 +0900

    css for sign up

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -5,21 +5,15 @@ function Signup() {
   return (
     <div className="signup">
       <h2>Sign up to I-Shadow</h2>
-      <form action="" method="POST">
-        <label htmlFor="">ID</label>
-        <br/>
-        <input type="text"/>
-        <br/>
-        <label htmlFor="">Password</label>
-        <br/>
-        <input type="password"/>
-        <br/>
-        <label htmlFor="">email</label>
-        <br/>
-        <input type="email"/>
-        <br/>
-        <button type="submit">Sign up</button>
-      </form>
+      <div className="signup-form">
+        <form action="" method="POST">
+          <label htmlFor="">Email</label> <br/>
+          <input type="email"/> <br/>
+          <label htmlFor="">Password</label> <br/>
+          <input type="password"/> <br/>
+          <button type="submit" className="btn_submit">Sign up</button>
+        </form>
+      </div>
     </div>
   );
 }
