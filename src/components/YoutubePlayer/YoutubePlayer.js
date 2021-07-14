81c20f888125be380bee86094bfa0585b53c8b5b
algosketch <sjy_9484@naver.com>
2021-07-14 19:05:55 +0900

    test recoding user voice

--- src/components/YoutubePlayer/YoutubePlayer.js
+++ src/components/YoutubePlayer/YoutubePlayer.js
@@ -1,17 +1,28 @@
 import './YoutubePlayer.css';
-import React from "react";
+import React, {useState} from "react";
+
 
 const youtubeCode = ['1eAZvWm0gE0', 'd-HK6DFi3MA']
 
 function YoutubePlayer({match}) {
+  const btnClick = () => {
+    console.log("player");/*
+    if(player){
+      player.seek(30);
+    }*/
+  };
+
   return (
     <div className="youtube-player">
-      <iframe width="1120" height="630" src={"https://www.youtube.com/embed/" + youtubeCode[1]} title="YouTube video player"
+
+      <iframe
+              width="1120" height="630" src={"https://www.youtube.com/embed/" + youtubeCode[1]} title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen>
       </iframe>
-      abd : {match.url}
+      param : {match.url}
+      <button onClick={btnClick}>123</button>
     </div>
   );
 }
