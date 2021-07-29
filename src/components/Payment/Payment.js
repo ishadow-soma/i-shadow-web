4feb41796a7a136b4f0c2e65a1e0b0210394daa2
algosketch <sjy_9484@naver.com>
2021-07-29 09:13:46 +0900

    css for payment

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,50 +1,43 @@
 import React from "react";
+import "./Payment.css"
 
 function Payment() {
   return (
     <div className="payment">
-      <h1>포인트 충전</h1>
-      <p>1포인트 = 1분짜리 영상을 변환할 수 있습니다.</p>
-      
-      <div>
-        <ul>
-          <li>
-            <p>
-              15point
-              <a href="">\1,000원 결제하기</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              45point
-              <a href="">\3,000원 결제하기</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              75point
-              <a href="">\5,000원 결제하기</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              1500point
-              <a href="">\10,000원 결제하기</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              450point
-              <a href="">\30,000원 결제하기</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              750point
-              <a href="">\50,000원 결제하기</a>
-            </p>
-          </li>
-        </ul>
+      <div className="container">
+        <div className="payment-header">
+          <h1>포인트 충전</h1>
+          <p className="description">1포인트 = 1분짜리 영상을 변환할 수 있습니다.</p>
+        </div>
+
+        <div className="payment-model">
+          <ul>
+            <li>
+              <p><span>15</span>point</p>
+              <a href="">1,000원 결제하기</a>
+            </li>
+            <li>
+              <p><span>45</span>point</p>
+              <a href="">3,000원 결제하기</a>
+            </li>
+            <li>
+              <p><span>75</span>point</p>
+              <a href="">5,000원 결제하기</a>
+            </li>
+            <li>
+              <p><span>150</span>point</p>
+              <a href="">10,000원 결제하기</a>
+            </li>
+            <li>
+              <p><span>450</span>point</p>
+              <a href="">30,000원 결제하기</a>
+            </li>
+            <li>
+              <p><span>750</span>point</p>
+              <a href="">50,000원 결제하기</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
