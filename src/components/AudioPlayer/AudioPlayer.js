c2553bb1f8d8b45b1523e04e2eb635462995a83c
algosketch <sjy_9484@naver.com>
2021-07-30 04:00:18 +0900

    add audio player

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,19 +1,57 @@
 import './AudioPlayer.css';
 import React from "react";
+import Header from "../../global/Header/Header";
+import Footer from "../../global/Footer/Footer";
 
 function AudioPlayer() {
   return (
-    <div className="audio-player">
-      <div className="audio-left">
-        <div className="audio-background">
+    <div className="wrap">
+      <Header/>
+      <div className="youtube-background">
+        <div className="container">
+          <div className="audio-player">
+            <div className="audio-frame">
+              <h2><i className="xi-headset"/> 내가 변환한 음성 콘텐츠</h2>
+              <h1>The Intern - Official Trailer [HD]</h1>
+              <span className="audio-background">
+                <span className="operation-menu">
+                  <i className="xi-step-backward"/>
+                  <i className="xi-pause pause"/>
+                  <i className="xi-step-forward"/>
+                </span>
+                <span className="volume">
+                  <i className="xi-volume-up"/>
+                </span>
+              </span>
+              <div className="caption">
+                Our hearts wore never broken
+              </div>
+              <Footer/>
+            </div>
 
+            <div className="script">
+              <button className="tab selected"><i className="xi-microphone tab-icon"/>Script</button>
+              <button className="tab"><i className="xi-microphone tab-icon"/> Rec.</button>
+
+              <div className="content">
+                <ul>
+                  <li>
+                    <button className="time-stamp">0:59</button>
+                    <p>Loving can hurt</p>
+                  </li>
+                  <li>
+                    <button className="time-stamp">1:23</button>
+                    <p>loving can hurt sometimes</p>
+                  </li>
+                  <li>
+                    <button className="time-stamp">1:56</button>
+                    <p>But it's the only thing that I know</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="caption">
-
-        </div>
-      </div>
-      <div className="script">
-
       </div>
     </div>
   );
