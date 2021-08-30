63be0585cd231b7c41c4a807ef619e435be8382a
algosketch <sjy_9484@naver.com>
2021-08-31 03:34:09 +0900

    add to patch user profile

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import "./EditProfile.css";
-import {user} from "global/store/store";
+import http, {user} from "global/store/store";
+import {getCookie} from "../../global/store/cookie";
+import axios from "axios";
 
-function EditProfile() {
+function EditProfile(props) {
   const email = user.email;
   const [name, setName] = useState(user.name);
   const [age, setAge] = useState(user.age);
@@ -15,6 +17,31 @@ function EditProfile() {
     setAge(e.target.value);
   }
 
+  const updateUser = () => {
+    axios({
+      method: "patch",
+      url: `${http.baseURL}users`,
+      headers: {"ACCESS-TOKEN": getCookie("jwt")},
+      body: {
+        "name": name,
+        "age": age,
+        "gender": "남성",
+        "purposeOfUse": "???"
+      }
+    }).then(res => {
+
+      if(res.data.success) {
+        console.log("user update success");
+        props.history.push("/");
+      }
+      else
+        console.log("user update fail");
+    }).catch(err => {
+      console.log("user update fail");
+      console.log(err);
+    })
+  }
+
   return (
     <div className="edit-profile">
       <div className="flex-left">
@@ -23,27 +50,25 @@ function EditProfile() {
           <span><i className="xi-at"/></span>
           <p>{email}</p>
         </div>
-        <form action="">
-          <div className="input">
-            <span><i className="xi-user-o"/></span>
-            <input type="text" value={name} placeholder="닉네임" onChange={handleChange}/>
+        <div className="input">
+          <span><i className="xi-user-o"/></span>
+          <input type="text" value={name} placeholder="닉네임" onChange={handleChange}/>
+        </div>
+        <div className="input">
+          <span><i className="xi-user"/></span>
+          <input type="text" placeholder="Age" value={age} onChange={ageChange}/>
+        </div>
+        <div className="sex-container">
+          <div className="sex-component">
+            <input type="radio" value="male" name="sex" className="sex-component"/>
+            <label htmlFor="">Male</label>
           </div>
-          <div className="input">
-            <span><i className="xi-user"/></span>
-            <input type="text" placeholder="Age" value={age} onChange={ageChange}/>
+          <div className="sex-component">
+            <input type="radio" value="female" name="sex" className="sex-component"/>
+            <label htmlFor="">Female</label>
           </div>
-          <div className="sex-container">
-            <div className="sex-component">
-              <input type="radio" value="male" name="sex" className="sex-component"/>
-              <label htmlFor="">Male</label>
-            </div>
-            <div className="sex-component">
-              <input type="radio" value="female" name="sex" className="sex-component"/>
-              <label htmlFor="">Female</label>
-            </div>
-          </div>
-          <button type="submit" className="btn-submit">Submit</button>
-        </form>
+        </div>
+        <button type="submit" className="btn-submit" onClick={updateUser}>Submit</button>
         <p className="or">or</p>
         <button id="resign">탈퇴하기</button>
       </div>
