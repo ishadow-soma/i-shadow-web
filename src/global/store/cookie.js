fa72fb7fc1951bc567b5016ad47fc555d2dcb1bb
algosketch <sjy_9484@naver.com>
2021-09-05 02:49:36 +0900

    refactor (add default setCookie)

--- src/global/store/cookie.js
+++ src/global/store/cookie.js
@@ -6,6 +6,14 @@ export const setCookie = (name, value, option) => {
   return cookies.set(name, value, { ...option })
 }
 
+export const setCookieDefault = (name, value) => {
+  setCookie(name, value, {
+    path: "/",
+    secure: true,
+    sameSite: "none"
+  });
+}
+
 export const getCookie = (name) => {
   return cookies.get(name)
 }
