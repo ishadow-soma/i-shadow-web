264e00ad53ab74eb439ee5012af7e70bda1196e7
algosketch <sjy_9484@naver.com>
2021-09-23 21:52:42 +0900

    add test for password validation

--- src/components/Signup/Signup.test.js
+++ src/components/Signup/Signup.test.js
@@ -1,4 +1,4 @@
-import { validateEmail } from "./Signup";
+import { validateEmail, validatePassword } from "./Signup";
 
 describe("Signup", () => {
   it("validateEmail", () => {
@@ -15,7 +15,7 @@ describe("Signup", () => {
     expect(result2).toBe(true);
   });
 
-  it("validateEmail: empty id", () => {
+  it("validateEmail - empty id", () => {
     // given
     const email1 = "@naver.com";
     const email2 = "@gmail.com";
@@ -29,7 +29,7 @@ describe("Signup", () => {
     expect(result2).toBe(false);
   });
 
-  it("validateEmail no @", () => {
+  it("validateEmail - no @", () => {
     // given
     const email1 = "abc1234naver.com";
     const email2 = "abcdegmail.com";
@@ -43,7 +43,7 @@ describe("Signup", () => {
     expect(result2).toBe(false);
   });
 
-  it("validateEmail invalid email domain", () => {
+  it("validateEmail - invalid email domain", () => {
     // given
     const email1 = "abc1234@naver";
     const email2 = "abcde@gmail";
@@ -61,5 +61,61 @@ describe("Signup", () => {
     expect(result2).toBe(false);
     expect(result3).toBe(false);
     expect(result4).toBe(false);
+  });
+
+  it("validatePassword", () => {
+    // given
+    const password1 = "abcd1234";
+    const password2 = "1234aaaabbbbcccc";
+
+    // when
+    const result1 = validatePassword(password1);
+    const result2 = validatePassword(password2);
+
+    // then
+    expect(result1).toBe(true);
+    expect(result2).toBe(true);
+  });
+
+  it("validatePassword - only alphabet", () => {
+    // given
+    const password1 = "abcdefgh";
+    const password2 = "aaaabbbbcccc";
+
+    // when
+    const result1 = validatePassword(password1);
+    const result2 = validatePassword(password2);
+
+    // then
+    expect(result1).toBe(false);
+    expect(result2).toBe(false);
+  });
+
+  it("validatePassword - only numbers", () => {
+    // given
+    const password1 = "11112222";
+    const password2 = "123456781234";
+
+    // when
+    const result1 = validatePassword(password1);
+    const result2 = validatePassword(password2);
+
+    // then
+    expect(result1).toBe(false);
+    expect(result2).toBe(false);
+  });
+
+  it("validatePassword - invalid length", () => {
+    // given
+    const password1 = "abc123";
+    const password2 = "abcdefg12345678901234567890";
+
+    // when
+    const result1 = validatePassword(password1);
+    const result2 = validatePassword(password2);
+
+    // then
+    expect(result1).toBe(false);
+    expect(result2).toBe(false);
   });
 });
