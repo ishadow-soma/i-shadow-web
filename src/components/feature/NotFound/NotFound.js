0451e87413d64a40eee05747bf9b1af25b9bd8ef
algosketch <sjy_9484@naver.com>
2021-10-21 23:12:49 +0900

    modify 404 page

--- src/components/feature/NotFound/NotFound.js
+++ src/components/feature/NotFound/NotFound.js
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="not-found container">
-      <h1>404 NOT FOUND</h1>
+    <div className="not-found">
+      <h1>404</h1>
       <h2>
-        <Link to="/">
-          <span>I shadow</span> 홈으로 돌아가기
-        </Link>
+        <Link to="/">홈으로 돌아가기</Link>
       </h2>
+      <br />
+      <p>
+        찾으시는 페이지가 존재하지 않습니다. <br />
+        페이지가 삭제되었거나 입력하신 URL 이 잘못되었습니다.
+      </p>
     </div>
   );
 }
