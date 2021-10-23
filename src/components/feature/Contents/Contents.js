8bf3328e80a5eb083dbc5680881275ab8c2b9523
algosketch <sjy_9484@naver.com>
2021-10-23 17:18:24 +0900

    layout mark up

--- src/components/feature/Contents/Contents.js
+++ src/components/feature/Contents/Contents.js
@@ -1,9 +1,20 @@
 import Header from "components/common/Header/Header";
+import React from "react";
 
 export default function Contents() {
   return (
     <div className="contents">
       <Header />
+      <div className="contents">
+        <div className="container">
+          <h1>CONTENTS</h1>
+          <div>
+            <div className="level-filter">난이도</div>
+            <div className="category-filter">카테고리</div>
+            <div className="content">영상들...</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
