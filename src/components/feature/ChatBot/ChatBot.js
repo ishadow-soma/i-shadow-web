01ef5e22215a1771e5fc33bf63525d7e4d59eeeb
algosketch <sjy_9484@naver.com>
2021-10-25 22:01:48 +0900

    add record into chat bot page

--- src/components/feature/ChatBot/ChatBot.js
+++ src/components/feature/ChatBot/ChatBot.js
@@ -2,12 +2,33 @@ import Header from "components/common/Header/Header";
 import "./ChatBot.css";
 import { Scrollbar } from "react-scrollbars-custom";
 import RecordedList from "../YoutubePlayer/RecordedList";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Dropdown from "react-dropdown";
 import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 import Footer from "../../common/Footer/Footer";
+import { FaStop } from "react-icons/fa";
+import Recorder from "../../../global/record/Recorder";
+import { repeatStore } from "../../../global/store/store";
 
 export default function ChatBot() {
+  // 녹음
+  const [recordingState, setRecordingState] = useState({
+    options: [],
+    defaultOption: null,
+    isRecording: false,
+  });
+  const recorder = new Recorder((flag) => {
+    setRecordingState({
+      options: recordingState.options,
+      defaultOption: recordingState.defaultOption,
+      isRecording: flag,
+    });
+  });
+
+  useEffect(() => {
+    recorder.setRecorder(setRecordingState);
+  }, []);
+
   return (
     <div className="wrap">
       <Header />
@@ -21,13 +42,28 @@ export default function ChatBot() {
             <div className="caption">
               <p id="caption">자막자막자막</p>
             </div>
-            <Dropdown
-              placeholder="Select an option"
-              className="dropdown"
-              placeholderClassName="test1"
-              arrowClosed={<AiOutlineArrowDown className="arrow-closed" />}
-              arrowOpen={<AiOutlineArrowUp className="arrow-open" />}
-            />
+            <div className="record-container">
+              <Dropdown
+                options={recordingState.options}
+                value={recordingState.defaultOption}
+                onChange={(item) => recorder.setAudioEnvironment(item.value)}
+                placeholder="Select an option"
+                className="dropdown"
+                placeholderClassName="test1"
+                arrowClosed={<AiOutlineArrowDown className="arrow-closed" />}
+                arrowOpen={<AiOutlineArrowUp className="arrow-open" />}
+              />
+              <div className="mic">
+                <i className="xi-microphone record-icon" id="record" />
+                <div className="record-icon">
+                  <FaStop id="stop" />
+                </div>
+                <div className="echo">
+                  <input type="checkbox" />
+                  <span>Echo</span>
+                </div>
+              </div>
+            </div>
             <Footer />
           </div>
           <div className="flex-right">
