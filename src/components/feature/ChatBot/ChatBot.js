a291c9ac8c97ed0afa59c7519891839727268cdc
algosketch <sjy_9484@naver.com>
2021-10-25 20:48:52 +0900

    add chat bot markup

--- src/components/feature/ChatBot/ChatBot.js
+++ src/components/feature/ChatBot/ChatBot.js
@@ -1,5 +1,9 @@
 import Header from "components/common/Header/Header";
 import "./ChatBot.css";
+import { Scrollbar } from "react-scrollbars-custom";
+import RecordedList from "../YoutubePlayer/RecordedList";
+import Bookmark from "../YoutubePlayer/Bookmark";
+import React from "react";
 
 export default function ChatBot() {
   return (
@@ -7,9 +11,33 @@ export default function ChatBot() {
       <Header />
       <div className="chat-bot">
         <div className="container">
-          <h1>Chat Bot</h1>
-          <div className="flex-left"></div>
-          <div className="flex-right"></div>
+          <div className="title">
+            <h1>Chat Bot</h1>
+          </div>
+          <div className="flex-left">
+            <div className="temp">임시로 만든 박스</div>
+            <div className="caption">
+              <p id="caption">자막자막자막</p>
+            </div>
+          </div>
+          <div className="flex-right">
+            <div className="tabs">
+              <button className="tab selected">
+                <i className="xi-file-text-o" />
+                Script
+              </button>
+              <button className="tab">
+                <i className="xi-microphone xi-x" /> Rec.
+              </button>
+            </div>
+
+            <div className="script">
+              <Scrollbar style={{ width: 400, height: 640 }}>
+                <ul id="script" />
+                <RecordedList />
+              </Scrollbar>
+            </div>
+          </div>
         </div>
       </div>
     </div>
