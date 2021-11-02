3d225a576a09a96b548b8df853392bf35389618d
algosketch <sjy_9484@naver.com>
2021-11-03 00:00:25 +0900

    add video list into contents page

--- src/components/feature/Contents/Contents.js
+++ src/components/feature/Contents/Contents.js
@@ -1,10 +1,57 @@
 import Header from "components/common/Header/Header";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "rc-slider";
 import { Range } from "rc-slider";
 import "./Contents.css";
+import VideoContents from "../MyRoom/VideoContents";
+import axios from "axios";
+import network from "../../../global/store/store";
+import { getCookie } from "../../../global/store/cookie";
+import logOnlyDevelopment from "../../../global/log/log";
 
 export default function Contents() {
+  const [contents, setContents] = useState([]);
+
+  useEffect(() => {
+    requestContents();
+  }, []);
+
+  async function requestContents() {
+    const res = await axios({
+      method: "get",
+      url: network.baseURL + "media",
+      headers: { "ACCESS-TOKEN": getCookie("jwt") },
+      params: {
+        categoryId: 20,
+        levelStart: 0,
+        leverEnd: 5,
+        page: 1,
+        videoType: 1,
+      },
+    });
+    if (process.env.NODE_ENV === "development")
+      logOnlyDevelopment(
+        "변환된 콘텐츠",
+        res.data.data.videoList.map((it) => {
+          return {
+            title: it.videoName,
+            thumbNailURL: it.thumbNailURL,
+            videoId: it.videoId,
+          };
+        })
+      );
+
+    setContents(
+      res.data.data.videoList.map((it) => {
+        return {
+          title: it.videoName,
+          thumbNailURL: it.thumbNailURL,
+          videoId: it.videoId,
+        };
+      })
+    );
+  }
+
   return (
     <div className="wrap">
       <Header />
@@ -88,7 +135,9 @@ export default function Contents() {
                 </span>
               </div>
             </div>
-            <div className="content">영상들...</div>
+            <div className="content">
+              <VideoContents videos={contents} videoType={0} />
+            </div>
           </div>
         </div>
       </div>
