9d9ca73a3ed2681d986a0ce73f88ccbf76a2d080
algosketch <sjy_9484@naver.com>
2021-11-05 04:27:21 +0900

    add : select category

--- src/components/feature/Contents/Contents.js
+++ src/components/feature/Contents/Contents.js
@@ -21,7 +21,7 @@ export default function Contents() {
       const res = await requestContents(i);
       setContents((prevContents) => [...prevContents, ...res]);
     }
-  }, []);
+  }, [category]);
 
   async function requestContents(page) {
     const res = await axios({
@@ -53,6 +53,12 @@ export default function Contents() {
   }
 
   const [values, setValues] = useState([1, 5]);
+
+  const onSelect = (target, categoryNumber = 20) => {
+    document.getElementsByClassName("selected")[0].classList.remove("selected");
+    target.classList.add("selected");
+    setCategory(categoryNumber);
+  };
 
   return (
     <div className="wrap">
@@ -112,62 +118,62 @@ export default function Contents() {
                 <p>카테고리 필터링</p>
               </div>
               <div className="categories">
-                <span className="item" onClick={(e) => onSelect(e.target)}>
+                <span className="item" onClick={(e) => onSelect(e.target, 1)}>
                   라이프 스타일
                 </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  게임
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  엔터테인먼트
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  취미
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
+                <span className="item" onClick={(e) => onSelect(e.target, 2)}>
                   음악/댄스
                 </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  여행/아웃도어
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  푸드/쿠킹
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  차/배/바이크
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
+                <span className="item" onClick={(e) => onSelect(e.target, 3)}>
                   뷰티/패션
                 </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  스포츠/건강
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  인물/유명인
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  경제/금융/재테크
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
+                <span className="item" onClick={(e) => onSelect(e.target, 4)}>
                   영화/애니메이션
                 </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  뉴스/정치/이슈
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  IT/기술/과학
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
-                  교육/강의
-                </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
+                <span className="item" onClick={(e) => onSelect(e.target, 5)}>
                   키즈
                 </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
+                <span className="item" onClick={(e) => onSelect(e.target, 6)}>
+                  게임
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 7)}>
+                  여행/아웃도어
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 8)}>
+                  스포츠/건강
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 9)}>
+                  뉴스/정치/이슈
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 10)}>
                   기관/단체/정부
                 </span>
-                <span className="item" onClick={(e) => onSelect(e.target)}>
+                <span className="item" onClick={(e) => onSelect(e.target, 11)}>
+                  엔터테인먼트
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 12)}>
+                  푸드/쿠킹
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 13)}>
+                  인물/유명인
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 14)}>
+                  IT/기술/과학
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 15)}>
                   동물/펫
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 16)}>
+                  취미
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 17)}>
+                  차/배/바이크
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 18)}>
+                  경제/금융/재테크
+                </span>
+                <span className="item" onClick={(e) => onSelect(e.target, 19)}>
+                  교육/강의
                 </span>
                 <span
                   className="item selected"
@@ -185,9 +191,4 @@ export default function Contents() {
       </div>
     </div>
   );
-}
-
-function onSelect(target) {
-  document.getElementsByClassName("selected")[0].classList.remove("selected");
-  target.classList.add("selected");
 }
